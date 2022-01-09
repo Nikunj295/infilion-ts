24cf2231c690dabbb1ddeea62a9af1ca6fdf2238
unknown <nikunj@lucentinnovation.co.in>
2022-01-09 20:14:59 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import "./styles/app.scss";
+
+const App: React.FC =  () => {
+    return (
+        <div className="hey">
+            APP
+        </div>
+    )
+}
+
+export default App
